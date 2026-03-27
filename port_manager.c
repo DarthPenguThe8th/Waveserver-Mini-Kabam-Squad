31ddda548a3df1edc4c61676a73f59745d497579
Nicolas K. <94948678+DarkMattrMaestro@users.noreply.github.com>
2026-03-26 21:04:58 -0400

    Add partial solution to (#8)

--- port_manager.c
+++ port_manager.c
@@ -145,6 +145,40 @@ void handle_delete_port(const udp_message_t *request, udp_message_t *response)
     response->status = STATUS_SUCCESS;
 }
 
+void handle_inject_fault(const udp_message_t *request, udp_message_t *response)
+{
+    // TODO
+    port_t *port = get_port_from_request(request, response);
+    if (!port) return;
+
+    if (!port->admin_enabled) {
+        response->status = STATUS_FAILURE;
+        return;
+    }
+
+    port->fault_active = true;
+    recalculate_oper_state(port);
+    LOG(LOG_INFO, "Port admin-disabled: port_idx=%d", port->id - 1);
+    response->status = STATUS_SUCCESS;
+}
+
+void handle_clear_fault(const udp_message_t *request, udp_message_t *response)
+{
+    // TODO
+    port_t *port = get_port_from_request(request, response);
+    if (!port) return;
+
+    if (!port->admin_enabled) {
+        response->status = STATUS_FAILURE;
+        return;
+    }
+
+    port->fault_active = false;
+    recalculate_oper_state(port);
+    LOG(LOG_INFO, "Port admin-disabled: port_idx=%d", port->id - 1);
+    response->status = STATUS_SUCCESS;
+}
+
 bool dispatch(const udp_message_t *req, udp_message_t *resp)
 {
     bool send_reply = true;
@@ -166,6 +200,12 @@ bool dispatch(const udp_message_t *req, udp_message_t *resp)
         break;
     case MSG_DELETE_PORT:
         handle_delete_port(req, resp);
+        break;
+    case MSG_INJECT_FAULT:
+        handle_inject_fault(req, resp);
+        break;
+    case MSG_CLEAR_FAULT:
+        handle_clear_fault(req, resp);
         break;
     default:
         LOG(LOG_WARN, "Unknown msg_type: %d", req->msg_type);
